bcd41ba364c70064a0b471677e74acb93f3d30a5
Marcel Knoth <M.Knoth@proLogistik.com>
2015-07-15 08:17:38 +0200

    Statistik_Gui angepasst. Unnötige Statistiken rausgenommen

--- Zeiterfassung/src/GUI/Statistik_Gui.java
+++ Zeiterfassung/src/GUI/Statistik_Gui.java
@@ -13,6 +13,9 @@ import java.awt.event.WindowEvent;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import javax.swing.border.TitledBorder;
+import java.awt.GridLayout;
+import javax.swing.UIManager;
 
 public class Statistik_Gui extends JFrame {
 
@@ -24,9 +27,6 @@ public class Statistik_Gui extends JFrame {
 	private JPanel contentPane;
 
 	private JLabel lbl_GesamtAZAusgabe;
-	private JLabel lbl_fAB;
-	private JLabel lbl_AnzahlPausen;
-	private JLabel lbl_dAPausen;
 	private JLabel lbl_ueberstunden_monat;
 
 	private JLabel lbl_ueberstunden_woche;
@@ -36,8 +36,7 @@ public class Statistik_Gui extends JFrame {
 	private JLabel lbl_arbeitsende_ohnePause;
 	
 	public Statistik_Gui(Rectangle bounds){
-		InitStatistik_GUI();
-		setBounds(bounds);
+		InitStatistik_GUI(bounds);
 		
 		System.out.println(Controller.getController().getTimeForLabel(Controller.getController().getEndTime()));
 	}
@@ -45,7 +44,8 @@ public class Statistik_Gui extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	private void InitStatistik_GUI() {
+	private void InitStatistik_GUI(Rectangle bounds) {
+		setBounds((int)bounds.getX(), (int)bounds.getY(), 450, 175);
 		SysTray.getSysTray(this);
 		setResizable(false);
 		setTitle("Statistik");
@@ -59,7 +59,7 @@ public class Statistik_Gui extends JFrame {
 			}
 		});
 
-		setBounds(100, 100, 513, 301);
+		setBounds(100, 100, 450, 175);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -67,78 +67,50 @@ public class Statistik_Gui extends JFrame {
 
 		// Gesamtarbeitszeit seit Datei erzeugt wurde
 		JLabel lbl_GesamtAZText = new JLabel("Gesamtarbeitszeit aktueller Monat (exkl. Heute):");
-		lbl_GesamtAZText.setBounds(10, 13, 282, 16);
+		lbl_GesamtAZText.setBounds(10, 110, 282, 16);
 		contentPane.add(lbl_GesamtAZText);
 
 		lbl_GesamtAZAusgabe = new JLabel("00:00");
 		// Ausgabe Gesamtarbeitszeit seit Datei erzeugt wurde
-		lbl_GesamtAZAusgabe.setBounds(304, 13, 191, 16);
+		lbl_GesamtAZAusgabe.setBounds(304, 110, 191, 16);
 		contentPane.add(lbl_GesamtAZAusgabe);
-		// lbl_GesamtAZAusgabe.setText(Controller.getController().getTimeForLabel(Controller.getController().gesamtAZ()));
-
-		JLabel lbl_fABText = new JLabel("Fr\u00FChester Arbeitsbeginn:");
-		lbl_fABText.setBounds(12, 94, 218, 16);
-		contentPane.add(lbl_fABText);
-
-		lbl_fAB = new JLabel("Keine Daten");
-		lbl_fAB.setBounds(308, 94, 251, 16);
-		contentPane.add(lbl_fAB);
-
-		JLabel lbl_AnzahlPausenText = new JLabel("Meisten Pausen an einem Tag:");
-		lbl_AnzahlPausenText.setBounds(10, 123, 218, 16);
-		contentPane.add(lbl_AnzahlPausenText);
-
-		lbl_AnzahlPausen = new JLabel("Pausen");
-		lbl_AnzahlPausen.setBounds(306, 123, 191, 16);
-		contentPane.add(lbl_AnzahlPausen);
-
-		JLabel lbl_dAnzahlPausenText = new JLabel(
-				"Durchschnittliche Anzahl Pausen:");
-		lbl_dAnzahlPausenText.setBounds(10, 152, 218, 16);
-		contentPane.add(lbl_dAnzahlPausenText);
-
-		lbl_dAPausen = new JLabel("Anzahl Pausen");
-		lbl_dAPausen.setBounds(306, 152, 191, 16);
-		contentPane.add(lbl_dAPausen);
-		
-		//JLabel label = new JLabel("\u00DCberstunden in der aktuellen Woche:");
-		JLabel label = new JLabel("\u00DCberstunden aktueller Monat (exkl. Heute):");
-		label.setBounds(10, 42, 282, 16);
-		contentPane.add(label);
-		
-		lbl_ueberstunden_monat = new JLabel("00:00");
-		lbl_ueberstunden_monat.setBounds(304, 42, 191, 16);
-		contentPane.add(lbl_ueberstunden_monat);
-		
-		JLabel lblberstundenAktuelleWoche = new JLabel("\u00DCberstunden aktuelle Woche (exkl. Heute):");
-		lblberstundenAktuelleWoche.setBounds(10, 71, 282, 16);
-		contentPane.add(lblberstundenAktuelleWoche);
-		
-		lbl_ueberstunden_woche = new JLabel((String) null);
-		lbl_ueberstunden_woche.setBounds(304, 71, 191, 16);
-		contentPane.add(lbl_ueberstunden_woche);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel.setBounds(10, 187, 485, 68);
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Arbeitsende (nach 8h)", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(10, 13, 282, 84);
 		contentPane.add(panel);
-		panel.setLayout(null);
+		panel.setLayout(new GridLayout(0, 2, 0, 0));
 		
-		JLabel lblArbeitsendeohnePause = new JLabel("Arbeitsende (ohne Pause):");
-		lblArbeitsendeohnePause.setBounds(12, 42, 244, 16);
-		panel.add(lblArbeitsendeohnePause);
-		
-		JLabel lblArbeitsende = new JLabel("Arbeitsende (min. 1/2 Stunde Pause):");
-		lblArbeitsende.setBounds(12, 13, 260, 16);
+		JLabel lblArbeitsende = new JLabel("Mit 1/2 Stunde Pause");
 		panel.add(lblArbeitsende);
 		
 		lbl_arbeitsende = new JLabel("00:00");
-		lbl_arbeitsende.setBounds(294, 13, 122, 16);
 		panel.add(lbl_arbeitsende);
 		
+		JLabel lblArbeitsendeohnePause = new JLabel("Ohne Pause");
+		panel.add(lblArbeitsendeohnePause);
+		
 		lbl_arbeitsende_ohnePause = new JLabel((String) null);
-		lbl_arbeitsende_ohnePause.setBounds(294, 42, 122, 16);
 		panel.add(lbl_arbeitsende_ohnePause);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "\u00DCberstunden", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(304, 13, 129, 84);
+		contentPane.add(panel_1);
+		panel_1.setLayout(new GridLayout(2, 2, 0, 0));
+		
+		JLabel lblberstundenAktuelleWoche = new JLabel("Woche:");
+		panel_1.add(lblberstundenAktuelleWoche);
+		
+		lbl_ueberstunden_woche = new JLabel((String) null);
+		panel_1.add(lbl_ueberstunden_woche);
+		
+		//JLabel label = new JLabel("\u00DCberstunden in der aktuellen Woche:");
+		JLabel lblAktuellerMonatexkl = new JLabel("Monat:");
+		panel_1.add(lblAktuellerMonatexkl);
+		
+		lbl_ueberstunden_monat = new JLabel("00:00");
+		panel_1.add(lbl_ueberstunden_monat);
 		
 		updateView();
 		setVisible(true);
@@ -158,10 +130,6 @@ public class Statistik_Gui extends JFrame {
 		double avgPausendauer = Controller.getController().getDAPausen();
 		avgPausendauer = (double) Math.round(avgPausendauer * 100) / 100;
 		int maxAnzahlPausen = Controller.getController().getMaxAnzahlPausen();
-		
-		lbl_fAB.setText(Controller.getController().findefAZ());
-		lbl_AnzahlPausen.setText(maxAnzahlPausen + " Pause" + (maxAnzahlPausen == 1 ? "" : "n"));
-		lbl_dAPausen.setText("" + avgPausendauer);
 		lbl_GesamtAZAusgabe.setText(Controller.getController().getTimeForLabel(Controller.getController().getGesamtMonatAZ()));
 		lbl_ueberstunden_monat.setText(Controller.getController().getTimeForLabel(Controller.getController().getUeberstundenMonat()));
 		lbl_ueberstunden_woche.setText(Controller.getController().getTimeForLabel(Controller.getController().getUeberstundenWoche()));
